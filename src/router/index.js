076adc6e28f3f2c2c6e5511fb278d221fb171025
smfarhad <farhad1556@gmail.com>
2023-01-26 09:32:09 +0600

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/HomeView.vue'
+import About from '../views/AboutView.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    Home
+    <p ref="p"> Hi {{ name }} {{ age }} how are you</p>
+    <button @click="handleEvent">Click Me</button>
+    <button @click="age++">{{ age }}</button>
+    <input type="type" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Home',
+  setup() {
+    console.log(this);
+    const p = ref(null);
+    let name = ref('mario')
+    let age = ref(31)
+    let handleEvent = () => {
+      name.value = "Farhad";
+      age.value = 25
+      // console.log(p, p.value);
+      // p.value.classList.add('test');
+      // p.value.textContent = 'hello Nano'
+    }
+    return { name, age, handleEvent, p }
+  }
+}
+</script>
