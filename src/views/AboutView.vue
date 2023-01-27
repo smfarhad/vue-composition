8daaa99d1717ca3598d77458e43441ec4791c0f1
smfarhad <farhad1556@gmail.com>
2023-01-27 09:15:46 +0600

    reactive

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    About View
+    <h1>About</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }}-{{ ninjaOne.age }}</p>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }}-{{ ninjaTwo.age }} - {{ nameTwo }} - </p>
+    <button @click="updateNinjaTwo">updateNinjaTwo</button>
   </div>
 </template>
+<script>
+import { ref, reactive } from "vue";
+export default {
+  name: 'About',
+  setup() {
+    const ninjaOne = ref({ name: 'Mario', age: 32 });
+    const ninjaTwo = reactive({ name: 'Toma', age: 35 });
+    let nameOne = ref('Mario');
+    let nameTwo = reactive('name22222');
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    }
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
+      nameTwo = 'Poki'
+    }
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameOne, nameTwo }
+  }
+}
+</script>
